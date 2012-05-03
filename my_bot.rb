a55e31e1117a28fb11cd548cfeddfec0c2d46563
Jrod <jarrod@jtms.net>
2012-05-03 17:44:03 -0400

    initial

---
+++ my_bot.rb
@@ -0,0 +1,23 @@
+$:.unshift File.dirname($0)
+require 'ants.rb'
+
+ai=AI.new
+
+ai.setup do |ai|
+	# your setup code here, if any
+end
+
+ai.run do |ai|
+  square_orders = []
+
+	ai.my_ants.each do |ant|
+    # try to go north, if possible; otherwise try east, south, west.
+    [:N, :E, :S, :W].each do |dir|
+      if ant.square.neighbor(dir).passable? && !square_orders.include?(ant.square.neighbor(dir))
+        square_orders << ant.square.neighbor(dir)
+        ant.order dir
+				break
+			end
+		end
+	end
+end
