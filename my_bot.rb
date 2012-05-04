95ec129eac49208587d5194f7c46dc450d2ba0f8
Jrod <jarrod@jtms.net>
2012-05-04 19:41:43 -0400

    refactor the "starter package" to make it more idiomatic ruby (and readable)

--- my_bot.rb
+++ my_bot.rb
@@ -10,12 +10,14 @@ end
 ai.run do |ai|
   square_orders = []
 
+  puts ai.enemy_ants.inspect
+
   ai.my_ants.each do |ant|
     # try to go north, if possible; otherwise try east, south, west.
     [:N, :E, :S, :W].each do |dir|
-      if ant.square.neighbor(dir).passable? && !square_orders.include?(ant.square.neighbor(dir))
-        square_orders << ant.square.neighbor(dir)
-        ant.order dir
+      if ant.square.neighbor(dir, ai).passable? && !square_orders.include?(ant.square.neighbor(dir, ai))
+        square_orders << ant.square.neighbor(dir, ai)
+        ant.order(dir, ai)
         break
       end
     end
